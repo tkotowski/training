b628cba4a48f2c70e72fb4d19fcb76828e3e2c7d
tkotowski <tomasz.kotowski95@gmail.com>
2016-07-26 10:39:37 +0200

    Hmmmmmmmmmm

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hasdasdasdasdasdasdello sadsadsadWorld!");
+        System.out.println("Hasdasdasdasdasdassdsadsadasdello sadsadsadWorld!");
     }
+
+    public static void cudo() {
+        System.out.println("Dzińdybsdsadsadry");
+    }
+
 }
+
